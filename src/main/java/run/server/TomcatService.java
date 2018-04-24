fc4ce35028f51454a9c57356da950ae6c2d0f117
Bear_nb <Bear_nb@BEAR_NB>
2018-04-24 14:32:18 +0800

    2018/04/24 BEAR test.java --> TomcatService.java 名稱修改

--- src/main/java/run/server/TomcatService.java
+++ src/main/java/run/server/TomcatService.java
@@ -6,9 +6,9 @@ import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class test {
+public class TomcatService {
 
-	private static Logger log = LoggerFactory.getLogger(test.class);
+	private static Logger log = LoggerFactory.getLogger(TomcatService.class);
 	
 	public static void main(String[] args) {
 		log.info("aaa");
